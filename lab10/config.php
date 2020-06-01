764c0ea9bff052d540ede3bece6381d42af3d40c
YC-Yuan <YC-Yuan@users.noreply.github.com>
2020-06-01 21:43:18 +0800

    lab10 1.0

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testUser');
+define('DBPASS', 'qwertyuiop');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
