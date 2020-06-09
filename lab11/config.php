09ee0f86a9fc804634ed207e6a1d04abf027de35
YC-Yuan <YC-Yuan@users.noreply.github.com>
2020-06-09 16:36:51 +0800

    lab11 1.0

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'testUser');
+define('DBPASS', 'qwertyuiop');
+define('DBCONNSTRING','mysql:dbname=Login;charset=utf8mb4;');
+?>
